f139498623fa16bd42f06657ab1a0a5e92255cf0
MrScript <62763562+MrScriptTV@users.noreply.github.com>
2022-09-12 23:41:17 +0200

    Create conditions_ex.py

---
+++ conditions_ex.py
@@ -0,0 +1,11 @@
+# Exercice conditions
+
+# Un magasin propose 3 produits
+# Un ordinateur à 1500.-
+# Un téléphone à 800.-
+# Un pack spécial -> ordinateur + téléphone à 2000.-
+
+# L'utilisateur rentre le montant de son porte-monnaie
+# Il veut s'acheter un des produits
+# En fonction du montant de son porte-monnaie
+# Vous devez lui proposer le meilleur produit
